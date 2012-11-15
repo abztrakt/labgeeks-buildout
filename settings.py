37af4dd62f230e236f32d5d29e5e7e08ebe34326
Jon Swanson <jswan2@uw.edu>
2012-11-14 16:22:48 -0800

    Altered project settings to work properly

--- settings.py
+++ settings.py
@@ -24,7 +24,7 @@ DATABASES = {
 }
 
 # User Profiles
-AUTH_PROFILE_MODULE = 'people.UserProfile'
+AUTH_PROFILE_MODULE = 'labgeeks_people.UserProfile'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
