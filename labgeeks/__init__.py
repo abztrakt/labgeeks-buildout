fcd49295e2f38b0fffac0f69d4f9124d852d1f9d
Jon Swanson <jswan2@uw.edu>
2013-01-07 14:51:34 -0800

    potential post-migrate script fix for apache

--- labgeeks/__init__.py
+++ labgeeks/__init__.py
@@ -6,10 +6,13 @@ def update_permissions_after_migration(app, **kwargs):
     Update app permission just after every migration.
     This is based on app django_extensions update_permissions management command.
     """
-    import settings
-    from django.db.models import get_app, get_models
-    from django.contrib.auth.management import create_permissions
+    try:
+        import settings
+        from django.db.models import get_app, get_models
+        from django.contrib.auth.management import create_permissions
 
-    create_permissions(get_app(app), get_models(), 2 if settings.DEBUG else 0)
+        create_permissions(get_app(app), get_models(), 2 if settings.DEBUG else 0)
+    except ImportError:
+        pass
 
 post_migrate.connect(update_permissions_after_migration)
