5bb7b8426b76f0be2352b1f5ea1f527cad0a7e99
Craig M. Stimmel <cstimmel@uw.edu>
2013-01-07 16:12:44 -0800

    Commented out code that makes the app fail when served by mod_wsgi. We'll have to find another way to do this.

--- labgeeks/__init__.py
+++ labgeeks/__init__.py
@@ -1,18 +1,19 @@
-from south.signals import post_migrate
-
-
-def update_permissions_after_migration(app, **kwargs):
-    """
-    Update app permission just after every migration.
-    This is based on app django_extensions update_permissions management command.
-    """
-    try:
-        import settings
-        from django.db.models import get_app, get_models
-        from django.contrib.auth.management import create_permissions
-
-        create_permissions(get_app(app), get_models(), 2 if settings.DEBUG else 0)
-    except ImportError:
-        pass
-
-post_migrate.connect(update_permissions_after_migration)
+# No worky
+#from south.signals import post_migrate
+#
+#
+#def update_permissions_after_migration(app, **kwargs):
+#    """
+#    Update app permission just after every migration.
+#    This is based on app django_extensions update_permissions management command.
+#    """
+#    try:
+#        import settings
+#        from django.db.models import get_app, get_models
+#        from django.contrib.auth.management import create_permissions
+#
+#        create_permissions(get_app(app), get_models(), 2 if settings.DEBUG else 0)
+#    except ImportError:
+#        pass
+#
+#post_migrate.connect(update_permissions_after_migration)
