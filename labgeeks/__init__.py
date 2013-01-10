800373d5fd2383d876d37ed40277955c38564c8f
Jon Swanson <jswan2@uw.edu>
2013-01-09 18:09:34 -0800

    SDEV-231: post_migrate in this __init__ sometimes causes 500 errors

--- labgeeks/__init__.py
+++
@@ -1,9 +0,0 @@
-from south.signals import post_migrate
-
-
-def update_permissions(**kwargs):
-    import sys
-    sys.stdout.write("If there are new content types or permissions added, remember to run-> labgeeks update_permissions\n\n")
-
-
-post_migrate.connect(update_permissions)
